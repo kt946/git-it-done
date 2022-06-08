9af785106485a8eabea3b34774aae04d0ac9dd6c
KT 2022 <103476893+kt946@users.noreply.github.com>
2022-06-08 00:23:02 -0400

    add complex queries

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -2,6 +2,7 @@ var userFormEl = document.querySelector("#user-form");
 var nameInputEl = document.querySelector("#username");
 var repoContainerEl = document.querySelector("#repos-container");
 var repoSearchTerm = document.querySelector("#repo-search-term");
+var languageButtonsEl = document.querySelector('#language-buttons');
 
 var formSubmitHandler = function(event) {
     event.preventDefault();
@@ -11,9 +12,20 @@ var formSubmitHandler = function(event) {
 
     if (username) {
         getUserRepos(username);
+        
         nameInputEl.value = "";
     } else {
         alert("Please enter a GitHub username");
+    }
+};
+
+var buttonClickHandler = function(event) {
+    var language = event.target.getAttribute("data-language");
+    if (language) {
+        getFeaturedRepos(language);
+
+        // clear old content
+        repoContainerEl.textContent = "";
     }
 };
 
@@ -37,6 +49,20 @@ var getUserRepos = function(user) {
             // Notice this '.catch()' getting chained onto the end of the '.then()' method
             alert("Unable to connect to GitHub");
         });
+};
+
+var getFeaturedRepos = function(language) {
+    var apiUrl = "https://api.github.com/search/repositories?q=" + language + "+is:featured&sort=help-wanted-issues";
+
+    fetch(apiUrl).then(function(response) {
+        if (response.ok) {
+            response.json().then(function(data) {
+                displayRepos(data.items, language);
+            });
+        } else {
+            alert('Error: Github User Not Found');
+        }
+    });
 };
 
 var displayRepos = function(repos, searchTerm) {
@@ -85,5 +111,7 @@ var displayRepos = function(repos, searchTerm) {
         repoContainerEl.appendChild(repoEl);
     }
 };
+
+languageButtonsEl.addEventListener("click", buttonClickHandler);
 
 userFormEl.addEventListener("submit", formSubmitHandler);
